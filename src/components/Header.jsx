916a92edf11b92ac5e585d99b35b6db1b8a33b4f
StefanPoprasic <stefanpoprasic25@gmail.com>
2025-03-03 22:58:50 +0100

    Investment calculator

---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+import Logo from '../assets/investment-calculator-logo.png'
+
+export default function Header(){
+    return <header id="header">
+        <img src={Logo} alt="Logo showing a money bag"/>
+        <h1>Investment calculator</h1>
+    </header>
+}
